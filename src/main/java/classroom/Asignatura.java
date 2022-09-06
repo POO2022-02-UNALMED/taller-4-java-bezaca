bfef04d6ba8f06cdd8ea8fd7e667cf8e2fa418c7
Felipe Cabeza <felipe.cabeza@gmfinancial.com>
2022-09-05 22:14:01 -0500

    done

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,11 +10,6 @@ public class Asignatura {
 
     Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
-    }
-
-    Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
     }
 
     Asignatura(int codigoExterno,Tipo tipo) {
@@ -39,8 +34,8 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
+    void cambiarDatos(double codigoInterno) {
+        setCodigoInterno(codigoInterno);
     }
 
     void cambiarDatos(int codigoExterno) {
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,14 +2,9 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
-    
-    static {
-        totalPersonas = 0;
-        cedula = 3;
-    }
 
     Persona(long cedula, String nombre) {
         this.cedula = cedula;
@@ -31,7 +26,12 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
+    }
+
+    Persona(){
+        this("", 0);
     }
     
     long getCedula() {
